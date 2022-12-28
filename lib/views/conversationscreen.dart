0f70e20752dacbb394c3be2167e66ab7ac5f7e3a
Gneya <80591287+Gneya@users.noreply.github.com>
2022-12-28 23:00:35 +0530

    UI of COnversation screen

--- lib/views/conversationscreen.dart
+++ lib/views/conversationscreen.dart
@@ -20,11 +20,13 @@ class ConversationScreen extends StatefulWidget {
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
+  bool isLoading = false;
   TextEditingController messageController = new TextEditingController();
   final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
       .collection("ChatRoom")
       .doc(Constant.chatRoomId)
       .collection("chats")
+      .orderBy("time")
       .snapshots();
 
   DatabaseMethods _databaseMethods = new DatabaseMethods();
@@ -34,28 +36,39 @@ class _ConversationScreenState extends State<ConversationScreen> {
         //  stream: chatMessageStream,
         stream: _usersStream,
         builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          return ListView.builder(
-              itemCount: snapshot.data!.docs.length,
-              itemBuilder: (ctx, index) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Text("Loading");
-                }
-
-                QuerySnapshot<Object?>? snap = snapshot.data; // Snapshot
-                List<DocumentSnapshot> items = snap!.docs; // List of Documents
-                DocumentSnapshot item = items[index];
-                return MessageTile(
-                  message: item['message'],
-                );
-              });
+          if (snapshot.data == null) {
+            return Center(
+                child: CircularProgressIndicator(
+              color: Colors.orange.shade600,
+            ));
+          } else {
+            return Container(
+              padding: EdgeInsets.only(top: 15),
+              height: MediaQuery.of(context).size.height / 1.2,
+              child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (ctx, index) {
+                    QuerySnapshot<Object?>? snap = snapshot.data; // Snapshot
+                    List<DocumentSnapshot> items =
+                        snap!.docs; // List of Documents
+                    DocumentSnapshot item = items[index];
+                    return MessageTile(
+                      message: item['message'],
+                      sendByMe:
+                          item['sendBy'] == Constant.myname ? true : false,
+                    );
+                  }),
+            );
+          }
         });
   }
 
   sendMessage() {
     if (messageController.text.isNotEmpty) {
-      Map<String, String> messageMap = {
+      Map<String, dynamic> messageMap = {
         "message": messageController.text,
-        "sendBy": Constant.myname
+        "sendBy": Constant.myname,
+        "time": DateTime.now().millisecondsSinceEpoch
       };
       _databaseMethods.addConversationMessage(widget.ChatRoomId, messageMap);
       messageController.text = "";
@@ -64,6 +77,11 @@ class _ConversationScreenState extends State<ConversationScreen> {
 
   @override
   void initState() {
+    // Future.delayed(Duration(seconds: 5), () {
+    //   setState(() {
+    //     isLoading = false;
+    //   });
+    // });
     // TODO: implement initState
     // print("Inside");
     // setState(() {
@@ -117,6 +135,7 @@ class _ConversationScreenState extends State<ConversationScreen> {
           child: Stack(
         children: [
           Container(
+            height: MediaQuery.of(context).size.height / 1.3,
             child: ChatMessageList(),
           ),
           Container(
@@ -125,8 +144,9 @@ class _ConversationScreenState extends State<ConversationScreen> {
               height: MediaQuery.of(context).size.height / 13,
               margin: EdgeInsets.only(top: 12, bottom: 22, left: 10, right: 10),
               decoration: BoxDecoration(
-                  color: Colors.grey.shade400.withOpacity(0.3),
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
+                color: Colors.grey.shade400.withOpacity(0.3),
+                //borderRadius: BorderRadius.all(Radius.circular(10))
+              ),
               padding: EdgeInsets.all(10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -162,12 +182,69 @@ class _ConversationScreenState extends State<ConversationScreen> {
 
 class MessageTile extends StatelessWidget {
   final String message;
-  const MessageTile({required this.message});
+  final bool sendByMe;
+  const MessageTile({required this.message, required this.sendByMe});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(message),
+      padding: sendByMe ? EdgeInsets.only(right: 5) : EdgeInsets.only(left: 10),
+      width: MediaQuery.of(context).size.width,
+      alignment: sendByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 2,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              //borderRadius: BorderRadius.all(Radius.circular(20)),
+              borderRadius: sendByMe
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    ),
+              gradient: sendByMe
+                  ? LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [
+                        0.1,
+                        0.6,
+                      ],
+                      colors: [
+                        Colors.orange.withOpacity(0.7),
+                        Colors.pinkAccent,
+                      ],
+                    )
+                  : LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [
+                        0.1,
+                        0.6,
+                      ],
+                      colors: [
+                        Colors.orange.withOpacity(0.2),
+                        Colors.pinkAccent.withOpacity(0.2),
+                      ],
+                    ),
+            ),
+            padding: EdgeInsets.all(15),
+            margin: EdgeInsets.all(2),
+            child: Text(
+              message,
+              style: TextStyle(color: sendByMe ? Colors.white : Colors.black),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
